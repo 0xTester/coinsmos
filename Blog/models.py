815275ad794b1ae6e1fd38e4546a0af4e7353de6
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-02-19 18:21:46 -0600

    Database migrations

--- Blog/models.py
+++ Blog/models.py
@@ -44,3 +44,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comentario(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
