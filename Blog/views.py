e22dacb7485f1d48ca139240852fed172711ed2e
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-02-19 18:54:30 -0600

    Prueba de comentarios 3

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comentario
-from django.views.generic import ListView, DetailView, Createview
+from django.views.generic import ListView, DetailView, CreateView
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
 from django.contrib.auth.models import User
