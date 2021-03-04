7561aaaaa0ba167032df49a0dedc299179f13957
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-03-04 15:27:10 -0600

    Intentando Deployear Price_Section4

--- PaginaCoinsmos/settings.py
+++ PaginaCoinsmos/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 
 
 # Celery
-CELERY_BROKER_URL= env('REDIS_URL')
-CELERY_RESULT_BACKEND=env('REDIS_URL')
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 #---------------------------------------------
 CKEDITOR_UPLOAD_PATH = "uploads/"
