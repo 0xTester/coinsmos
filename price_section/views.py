1756182fde2b8adeb658fb0f4d366c6d5c94cee0
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-03-07 21:20:05 -0600

    Final Price_Tracker3

--- price_section/views.py
+++ price_section/views.py
@@ -9,6 +9,6 @@ from .models import Crypto
 class CryptoView(ListView):
     model = Crypto
     template_name = 'price_section/tracker.html'
-    def get_queryset(self):
-        cryptos = Crypto.objects.order_by('created_at')
-        return cryptos
+    #def get_queryset(self):
+        #cryptos = Crypto.objects.order_by('created_at')
+        #return cryptos
