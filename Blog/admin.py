062f7d1a3b9df92a69555396c938f40fb1f1489c
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-06-05 17:17:40 -0500

    Bloquear autores5

--- Blog/admin.py
+++ Blog/admin.py
@@ -5,12 +5,13 @@ from django.db import models
 # Register your models here.
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','author', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'contenido']
     prepopulated_fields = {'slug': ('title',)}
-
+    actions = ['dispatch']
     #def get_queryset(self, request):
         #qs = super(PostAdmin, self).get_queryset(request)
         #return qs.filter(author=request.user)
