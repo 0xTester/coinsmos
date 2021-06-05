e9fd2e0fef778bee30ed76905047918f20682802
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-06-05 16:56:54 -0500

    Bloquear autores2

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,23 +1,19 @@
 from django.contrib import admin
 from .models import Post, Comentario
-from django.core.exceptions import PermissionDenied
+
 from django.db import models
 # Register your models here.
 
 
-class PostAdmin(admin.ModelAdmin,LoginRequiredMixin, edit.UpdateView):
+class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','author', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'contenido']
     prepopulated_fields = {'slug': ('title',)}
 
-    def dispatch(self, request, *args, **kwargs):
-        handler = super().dispatch(request, *args, **kwargs)
-        user = request.user
-        post = self.get_object()
-        if not (post.author == user or user.is_superuser):
-            raise PermissionDenied
-        return handler
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(author=request.user)
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comentario)
