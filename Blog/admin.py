d4c1bee7e1033805d890c8cd3cf47f75b532e470
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-06-05 17:09:55 -0500

    Bloquear autores4

--- Blog/admin.py
+++ Blog/admin.py
@@ -3,8 +3,8 @@ from .models import Post, Comentario
 
 from django.db import models
 # Register your models here.
-
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','author', 'created_on')
     list_filter = ("status",)
@@ -14,6 +14,12 @@ class PostAdmin(admin.ModelAdmin):
     #def get_queryset(self, request):
         #qs = super(PostAdmin, self).get_queryset(request)
         #return qs.filter(author=request.user)
-
+    def dispatch(self, request, *args, **kwargs):
+        handler = super().dispatch(request, *args, **kwargs)
+        user = request.user
+        post = self.get_object()
+        if not (post.author == user):
+            raise PermissionDenied
+        return handler
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comentario)
