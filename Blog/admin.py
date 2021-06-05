81ed1abe4e5e9711a6ce09d35326772712745a33
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-06-05 18:39:26 -0500

    Bloquear autores10

--- Blog/admin.py
+++ Blog/admin.py
@@ -11,10 +11,17 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("status",)
     search_fields = ['title', 'contenido']
     prepopulated_fields = {'slug': ('title',)}
-    obj.author = request.user
+    def formfield_for_choice_field(self, db_field, request, **kwargs):
+        if db_field.name == "status":
+            kwargs['choices'] = (
+                ('accepted', 'Accepted'),
+                ('denied', 'Denied'),
+            )
+        
+        return super().formfield_for_choice_field(db_field, request, **kwargs)
     #def get_queryset(self, request):
         #qs = super(PostAdmin, self).get_queryset(request)
         #return qs.filter(author=request.user)
-    
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comentario)
