b12edb6388ac6aadf484773794150dcbbc9eee52
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-06-05 23:25:26 -0500

    Bloquear autores10

--- Blog/admin.py
+++ Blog/admin.py
@@ -16,20 +16,9 @@ class PostAdmin(admin.ModelAdmin):
     #    qs = super(PostAdmin, self).get_queryset(request)
     #    return qs.filter(author=request.user)
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "author":
-            kwargs["initial"] = request.user.id
-            return db_field.formfield(**kwargs)
-        return super(PostAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
-
-    def has_change_permission(self, request, obj=None):
-        has_class_permission = super(PostAdmin, self).has_change_permission(request, obj)
-
-        if not has_class_permission:
-            return False
-
-        if obj is not None and not request.user.is_superuser and request.user.id != obj.author.id:
-            return False
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comentario)
