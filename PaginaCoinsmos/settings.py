2e7311b1a6fb124c3e2179e12a84dcca3bed1db8
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-06-19 13:15:59 -0500

    Justificacion6

--- PaginaCoinsmos/settings.py
+++ PaginaCoinsmos/settings.py
@@ -176,6 +176,7 @@ DEFAULT_FILE_STORAGE = 'PaginaCoinsmos.storages.MediaStore'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals(), staticfiles=False)
