1a5c857a900811e3af38af9ccc9b7367de403562
Alberto Gonzalez <alberto.coinastro@gmail.com>
2021-06-19 13:21:04 -0500

    Justificacion6

--- PaginaCoinsmos/settings.py
+++ PaginaCoinsmos/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG_VALUE') == False
 
-ALLOWED_HOSTS = ['coinsmosoficial.herokuapp.com']
+ALLOWED_HOSTS = ['coinsmos.com']
 
 
 # Application definition
